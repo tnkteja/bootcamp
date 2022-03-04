01deda509d497c0bcb730dd7cb04fc84633467ad
Krishna Teja <krishnateja@HEALTHIs-MacBook-Pro.local>
2022-03-04 11:53:56 +0530

    testing practice

--- testing practice/t.js
+++ testing practice/t.js
@@ -12,4 +12,27 @@ export function reverseString(string) {
     return string;
 }
 
-export calculator
+export let calculator = {
+    add: (a,b) => a+b,
+    subtract: (a,b) => a-b,
+    divide: (a,b) =>  a/b,
+    multiply: (a,b) => a*b,
+}
+
+export function caesarCipher(string) {
+    let newstring='';
+    for( let char of string) {
+        newstring+=  String.fromCharCode(char.charCodeAt()+1)
+    }
+    return newstring;
+}
+
+export function analyzeArray(array) {
+    let sum = array.reduce((a,i)=> a+i);
+    return {
+        average: sum/array.length,
+        min: Math.min(...array),
+        max: Math.max(...array),
+        length: array.length
+    }
+}
--- testing practice/t.test.js
+++ testing practice/t.test.js
@@ -1,6 +1,6 @@
-import { expect } from "@jest/globals";
+import { expect, it } from "@jest/globals";
 import { test } from "jest-circus";
-import {capitalize, reverseString} from "./t";
+import {capitalize, reverseString, calculator, caesarCipher, analyzeArray} from "./t";
 
 it("camel c capitalized is Camel", () => {
     expect(capitalize("camel")).toBe("Camel");
@@ -9,3 +9,32 @@ it("camel c capitalized is Camel", () => {
 it("camel reversed is lemac", ()=> {
     expect(reverseString("camel")).toBe("lemac")
 });
+
+it("1 + 1 = 2", ()=> {
+expect(calculator.add(1,1)).toEqual(2);
+})
+
+it("1 - 1 = 0", ()=> {
+    expect(calculator.subtract(1,1)).toBe(0);
+})
+
+it("10 / 2 = 5", () => {
+    expect(calculator.divide(10,2)).toBe(5);
+} )
+
+it("2 * 5 = 10", ()=> {
+    expect(calculator.multiply(2,5)).toEqual(10);
+})
+
+it("abc caesar is bcd", ()=> {
+    expect(caesarCipher("abc")).toBe("bcd");
+})
+
+it("[1,8,3,4,2,6] average is 4, min is 1, max is 8, length is 6", ()=> {
+    let array = [1,8,3,4,2,6];
+    let result = analyzeArray(array);
+    expect(result.average).toBe(4);
+    expect(result.min).toBe(1);
+    expect(result.max).toBe(8);
+    expect(result.length).toBe(6);
+})
